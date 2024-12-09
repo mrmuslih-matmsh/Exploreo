c3011a8a16904f088f2081825539bad6e5e9eb98
aafith <itzaffu1234@gmail.com>
2024-12-09 14:23:02 +0530

    Home Screen Titlr update

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -10,6 +10,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Home Screen'),
+      ),
+    );
   }
 }
